0ff26fc48425eaf3c84f313dc3445287862e2331
mayowa-kalejaiye <kalejaiyemayowa3@gmail.com>
2026-02-04 18:36:12 +0100

    feat: enhance global JSX types by declaring ThreeElements from @react-three/fiber

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,15 @@
-import { ThreeElements } from '@react-three/fiber'
+/// <reference types="@react-three/fiber" />
+
+import type { ThreeElements } from '@react-three/fiber'
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {}
+}
 
 declare global {
   namespace JSX {
     interface IntrinsicElements extends ThreeElements {}
   }
 }
+
+export {}
